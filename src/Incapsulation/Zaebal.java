6c35b4fa3a9ae6ab431c247ff1d05f9cf7af1fad
dsandul27-boop <dsandul27@gmail.com>
2026-02-14 17:08:07 +0200

    am demonstrat capacitatile incapsularii

---
+++ src/Incapsulation/Zaebal.java
@@ -0,0 +1,16 @@
+package Incapsulation;
+
+public class Zaebal {
+    private String name;
+
+    public Zaebal(String name){
+        this.name=name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import Incapsulation.Zaebal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Zaebal civil = new Zaebal("Otiebisi");
+        civil.setName("Vorbeste frumos");
+        System.out.println(civil.getName());
     }
 }
+
+
+//git add .
+//git commit -m "Incapsulare"
+//git push
